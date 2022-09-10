88e8b6e1020a068944594d892b99ef8ddf269adb
yohelce <yohel.cruz.espinoza@gmail.com>
2022-09-09 23:12:42 -0500

    Added 1-hbnb_route.py

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Module Hello route of Flask """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """ Print first message in HTML """
+    return 'Hello HBNB!'
+
+
+@app.route("/hbnb", stric_slashes=False)
+def hbnb():
+    """ Returns a string at the /hbnb/ route """
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000", debug=True)
